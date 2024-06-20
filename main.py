e03650961c56f5294888d157d478a16303fc50f1
tomyyyD <thomas5d@comcast.net>
2024-06-20 13:09:01 -0400

    sending instead of recieving

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import busio
 import board
 import digitalio
+import asyncio
 from lib.rfm9x import RFM9x
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
@@ -11,8 +12,8 @@ reset = digitalio.DigitalInOut(board.D25)
 cs.switch_to_output(value=True)
 reset.switch_to_output(value=True)
 
-radio_DIO0 = digitalio.DigitalInOut(board.RF_IO0)
-radio_DIO0.switch_to_input()
+# radio_DIO0 = digitalio.DigitalInOut(board.RF_IO0)
+# radio_DIO0.switch_to_input()
 
 radio = RFM9x(
     spi,
@@ -22,5 +23,5 @@ radio = RFM9x(
     baudrate=5000000
 )
 
-radio.dio0 = radio_DIO0
-radio.listen()
+# radio.dio0 = radio_DIO0
+radio.send(b'hello')
