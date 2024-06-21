38ecdaad198ffcd366525505a0dc0bf13e3f6fbd
Thomas Damiani <thomas5d@comcast.net>
2024-06-21 17:44:24 -0400

    partialy unpacking beacon

---
+++ lib/radio_utils.py
@@ -0,0 +1,26 @@
+import datetime
+import sys
+
+
+def hard_exit(radio, signum, fram):
+    radio.close()
+    sys.exit(0)
+
+
+def unpack_message(self, msg):
+    # Get the current time
+    current_time = datetime.datetime.now()
+    # Format the current time
+    formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S\n")
+    formatted_time = formatted_time.encode('utf-8')
+
+    header_info = (f"Header To: {msg.header_to}," +
+                   f"Header From: {msg.header_from}," +
+                   f"Header ID: {msg.header_id}," +
+                   f"Header Flags: {msg.header_flags}," +
+                   f"RSSI: {msg.rssi}," +
+                   f"SNR: {msg.snr}\n")
+    header_info = header_info.encode('utf-8')
+    payload = f"Payload: {msg.message}\n\n"
+    payload = payload.encode('utf-8')
+    print(header_info)
--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import signal
-import sys
 
 from lib.argus_lora import LoRa, ModemConfig
+from lib.radio_utils import hard_exit, unpack_message
 from lib.radiohead import RadioHead
-
-
-def hard_exit(radio, signum, fram):
-    radio.close()
-    sys.exit(0)
-
 
 radio = LoRa(0, 19, 25, modem_config=ModemConfig.Bw125Cr45Sf128, acks=False, freq=433)
 radiohead = RadioHead(radio, 1)
@@ -20,3 +14,4 @@ while True:
     msg = radiohead.receive_message()
     if msg is not None:
         print(msg)
+        print(unpack_message)
