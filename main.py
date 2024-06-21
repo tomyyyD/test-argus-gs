3f2e238835710c123b12458982304ed37d94790a
Thomas Damiani <thomas5d@comcast.net>
2024-06-21 16:51:09 -0400

    actually exit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import signal
+import sys
+
 from lib.argus_lora import LoRa, ModemConfig
 from lib.radiohead import RadioHead
-import signal
 
 
 def hard_exit(radio, signum, fram):
     radio.close()
+    sys.exit(0)
 
 
-radio = LoRa(0, 19, 25, modem_config=ModemConfig.Bw125Cr45Sf128, acks=False, freq=915)
+radio = LoRa(0, 19, 25, modem_config=ModemConfig.Bw125Cr45Sf128, acks=False, freq=433)
 radiohead = RadioHead(radio, 1)
 
 signal.signal(signal.SIGINT, lambda signum, frame: hard_exit(radio, signum, frame))
