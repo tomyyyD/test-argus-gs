05951c90d5fb7a0ecd1a613df90734b2d971e338
Thomas Damiani <thomas5d@comcast.net>
2024-06-21 16:44:12 -0400

    adding interrupt signal

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from lib.argus_lora import LoRa, ModemConfig
 from lib.radiohead import RadioHead
+import signal
+
+
+def hard_exit(radio, signum, fram):
+    radio.close()
+
 
 radio = LoRa(0, 19, 25, modem_config=ModemConfig.Bw125Cr45Sf128, acks=False, freq=915)
-
 radiohead = RadioHead(radio, 1)
+
+signal.signal(signal.SIGINT, lambda signum, frame: hard_exit(radio, signum, frame))
 
 while True:
     print("receiving...")
