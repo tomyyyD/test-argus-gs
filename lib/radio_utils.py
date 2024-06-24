63d04def350a34cb6c2766c43a4fcb3a28bab3a6
Thomas Damiani <thomas5d@comcast.net>
2024-06-24 11:28:23 -0400

    adjusting for system status bytes, interpret IMU heartbeat

--- lib/radio_utils.py
+++ lib/radio_utils.py
@@ -56,7 +56,9 @@ def unpack_message(msg):
     payload = f"Payload: {msg.message}\n\n"
     payload = payload.encode('utf-8')
     (ack, msg_id, msg_seq_count, msg_size) = unpack_header(msg)
-    data = msg.message[4:]
+    system_status = f"{msg.message[5]}, {msg.message[6]}"
+    print(system_status)
+    data = msg.message[6:]
     print(header_info)
     if msg_id == Definitions.SAT_HEARTBEAT_BATT:
         print("battery heartbeat")
@@ -70,13 +72,13 @@ def unpack_message(msg):
         byte 6: time mid low
         byte 7: time low
         """
-        batt_soc = data[0]
-        current = data[1] << 8 | data[2]
-        boot_count = data[3]
-        time = (data[4] << 24 |
-                data[5] << 16 |
-                data[6] << 8 |
-                data[7])
+        batt_soc: int = data[0]
+        current: int = data[1] << 8 | data[2]
+        boot_count: int = data[3]
+        time: int = (data[4] << 24 |
+                     data[5] << 16 |
+                     data[6] << 8 |
+                     data[7])
         print(f"battery soc: {batt_soc}")
         print(f"current: {current}")
         print(f"boot count: {boot_count}")
@@ -87,17 +89,92 @@ def unpack_message(msg):
         byte 0: sun vector x
         byte 1: sun vector y
         byte 2: sun vector 1
-        bytes 3-6: time
+        bytes 3-7: time
         """
-        sun_vec_x = data[0]
-        sun_vec_y = data[1]
-        sun_vec_z = data[2]
+        sun_vec_x = convert_floating_point_hp(data[0])
+        sun_vec_y = convert_floating_point_hp(data[1])
+        sun_vec_z = convert_floating_point_hp(data[2])
 
-        time = (data[4] << 24 |
-                data[5] << 16 |
-                data[6] << 8 |
-                data[7])
+        time: int = (data[4] << 24 |
+                     data[5] << 16 |
+                     data[6] << 8 |
+                     data[7])
         print(f"sun vector: ({sun_vec_x}, {sun_vec_y}, {sun_vec_z})")
         print(f"time: {time}")
     elif msg_id == Definitions.SAT_HEARTBEAT_IMU:
         print("imu heartbeat")
+        """
+        byte 0-4: mag x
+        byte 4-8: mag y
+        byte 8-12: mag z
+        byte 12-16: gyro x
+        byte 16-20: gyro y
+        byte 20-24: gyro z
+        byte 24-28: time
+        """
+        mag_x = convert_floating_point(data[0:4])
+        mag_y = convert_floating_point(data[4:8])
+        mag_z = convert_floating_point(data[8:12])
+
+        gyro_x = convert_floating_point(data[12:16])
+        gyro_y = convert_floating_point(data[16:20])
+        gyro_z = convert_floating_point(data[20:24])
+
+        time: int = (data[24] << 24 |
+                     data[25] << 16 |
+                     data[26] << 8 |
+                     data[27])
+
+        print(f"mag: ({mag_x}, {mag_y}, {mag_z})")
+        print(f"gyro: ({gyro_x}, {gyro_y}, {gyro_z})")
+        print(f"time: {time}")
+
+
+def convert_floating_point(message_list):
+    """
+    :param message_list: Byte list to convert to floating
+    :return: value as floating point
+
+    Convert FP value back to floating point
+    Range: [-32767.9999], 32767.9999]
+    """
+    val = 0
+    neg_bit_flag = 0
+
+    # Check -ve, extract LSB bytes for val, combine
+    if ((message_list[0] >> 7) == 1):
+        message_list[0] &= 0x7F
+        neg_bit_flag = 1
+
+    # Extract bytes for val, combine
+    val += (message_list[0] << 8) + message_list[1]
+    val += ((message_list[2] << 8) + message_list[3]) / 65536
+    if (neg_bit_flag == 1):
+        val = -1 * val
+
+    return val
+
+
+def convert_floating_point_hp(message_list):
+    """
+    :param message_list: Byte list to convert to floating
+    :return: value as floating point
+
+    Convert HP FP value back to floating point
+    Range: [-128.9999999, 128.9999999]
+    """
+    val = 0
+    neg_bit_flag = 0
+
+    # Check -ve, extract LSB bytes for val, combine
+    if ((message_list[0] >> 7) == 1):
+        message_list[0] &= 0x7F
+        neg_bit_flag = 1
+
+    # Extract bytes for val, combine
+    val += message_list[0]
+    val += ((message_list[1] << 16) + (message_list[2] << 8) + message_list[3] + 1) / 16777216
+    if (neg_bit_flag == 1):
+        val = -1 * val
+
+    return val
