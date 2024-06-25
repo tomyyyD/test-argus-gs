09e6167fd706b06f76b704026f5a19b42b79ae35
tomyyyD <thomas5d@comcast.net>
2024-06-25 14:45:42 -0400

    sql syntax error

--- lib/mysql_server_db.py
+++ lib/mysql_server_db.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from constants import Message_IDS
+from lib.constants import Message_IDS
 
 
 class Database:
@@ -23,7 +23,7 @@ class Database:
     def upload_sun(self, time, data):
         (sun_x, sun_y, sun_z) = data
         upload = (time, sun_x, sun_y, sun_z)
-        sql = "INSERT INTO sun (time, x, y, z) (%s, %s, %s, %s)"
+        sql = "INSERT INTO sun (time, x, y, z) VALUES (%s, %s, %s, %s)"
         try:
             self.cursor.execute(sql, upload)
         except Exception as e:
@@ -32,7 +32,7 @@ class Database:
     def upload_batt(self, time, data):
         (batt_soc, current, boot_count) = data
         upload = (time, batt_soc, current, boot_count)
-        sql = "INSERT INTO battery (time, batt_soc, current, boot_count)( %s, %s, %s, %s)"
+        sql = "INSERT INTO battery (time, batt_soc, current, boot_count) VALUES ( %s, %s, %s, %s)"
         try:
             self.cursor.execute(sql, upload)
         except Exception as e:
@@ -42,7 +42,7 @@ class Database:
         (mag_x, mag_y, mag_z,
          gyro_x, gyro_y, gyro_z) = data
         upload = (time, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z)
-        sql = "INSERT INTO imu (time, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z) (%s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO imu (time, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         try:
             self.cursor.execute(sql, upload)
         except Exception as e:
