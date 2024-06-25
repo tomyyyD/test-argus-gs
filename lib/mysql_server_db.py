af5ea8acc1353fd3f16a406e4670943917cfaae5
tomyyyD <thomas5d@comcast.net>
2024-06-25 14:51:32 -0400

    try catch for init

--- lib/mysql_server_db.py
+++ lib/mysql_server_db.py
@@ -4,13 +4,16 @@ from lib.constants import Message_IDS
 
 class Database:
     def __init__(self) -> None:
-        self.client = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="rexlab",
-            database="heartbeats",
-        )
-        self.cursor = self.client.cursor()
+        try:
+            self.client = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="rexlab",
+                database="heartbeats",
+            )
+            self.cursor = self.client.cursor()
+        except Exception as e:
+            print(f"could not connect to db: {e}")
 
     def upload_data(self, type, time, data):
         if type == Message_IDS.SAT_HEARTBEAT_SUN:
@@ -26,6 +29,7 @@ class Database:
         sql = "INSERT INTO sun (time, x, y, z) VALUES (%s, %s, %s, %s)"
         try:
             self.cursor.execute(sql, upload)
+            print("uploaded to sun table")
         except Exception as e:
             print(f"[ERROR]: Could not upload sun heartbeat: {e}")
 
@@ -35,6 +39,7 @@ class Database:
         sql = "INSERT INTO battery (time, batt_soc, current, boot_count) VALUES ( %s, %s, %s, %s)"
         try:
             self.cursor.execute(sql, upload)
+            print("uploaded to battery table")
         except Exception as e:
             print(f"[ERROR]: Could not upload battery heartbeat: {e}")
 
@@ -45,5 +50,6 @@ class Database:
         sql = "INSERT INTO imu (time, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         try:
             self.cursor.execute(sql, upload)
+            print("uploaded to imu table")
         except Exception as e:
             print(f"[ERROR]: Could not upload imu heartbeat: {e}")
